b31514a437174e1bfa1eb7f25870ab7262aee67e
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2014-03-02 09:48:49 +0900

    正式にString, std::stringの処理に変更。

--- 20140228_Java_String_Compare/Compare.java
+++ 20140228_Java_String_Compare/Compare.java
@@ -8,30 +8,31 @@ import java.util.Random;
 
 class Compare
 {
-	static int l = 1000 * 10000;
-	static int m = 2 * 10000;
-	static int n = 64;
+	static int m = 1000 * 10000;
+	static int n = 2 * 10000;
 
 	public static void
 	main(String arguments[]) {
 		// prepare
-		ArrayList<Character> data = new ArrayList<Character>();
+		ArrayList<String> data = new ArrayList<String>();
 		Random random = new Random(System.currentTimeMillis());
+		int h;
 		int k = m;
 		if (k < n) k = n;
-		for (int j = 0; j < k; ++j) {
-			data.add((char)random.nextInt(k));
+		for (int i = 0; i < k; ++i) {
+			if (i % 10000 == 0) System.out.print("+");
+			h = random.nextInt(k);
+			data.add(String.format("%X", h));
 		}
+		System.out.println("");
 		// O(n^3)
 		long x = 0;
-		for (int h = 0; h < l; ++h) {
-			if (h % 1000 == 0) System.out.print(".");
+		for (int i = 0; i < m; ++i) {
+			if (i % 10000 == 0) System.out.print(".");
 			// O(n^2)
-			for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < n; ++j) {
 				// O(n^1)
-				for (int j = 0; j < n; ++j) {
-					if (data.get(i) < data.get(j)) ++x;
-				}
+				if (data.get(i) == data.get(j)) ++x;
 			}
 		}
 		System.out.printf("[%d]\n", x);
--- 20140301_C++_String_Compare/sample.cpp
+++ 20140301_C++_String_Compare/sample.cpp
@@ -4,36 +4,41 @@
  */
 
 #include <vector>
+#include <string>
 #include <ctime>
 #include <cstdlib>
+#include <cstdio>
 
 int
 main()
 {
-	size_t l = 1000u * 10000u;
-	size_t m = 2u * 10000u;
-	size_t n = 64u;
+	size_t m = 1000u * 10000u;
+	size_t n = 2u * 10000u;
+
+	char buffer[1024];
 
 	// prepare
-	std::vector<char> data;
+	std::vector<std::string> data;
+	int h;
 	size_t k(m);
 	if (k < n) k = n;
-	data.reserve(k);
 	std::srand((unsigned)std::time(0));
-	for (size_t j(0); j < k; ++j) {
-		data.push_back((char)std::rand());
+	for (size_t i(0); i < k; ++i) {
+		if (i % 10000u == 0) std::printf("+");
+		h = std::rand();
+		sprintf(buffer, "%X", h);
+		data.push_back(std::string(buffer));
 	}
+	std::printf("\n");
 
 	// O(n^3)
 	size_t x(0);
-	for (size_t h(0); h < l; ++h) {
-		if (h % 1000u == 0) std::printf(".");
+	for (size_t i(0); i < m; ++i) {
+		if (i % 10000u == 0) std::printf(".");
 		// O(n^2)
-		for (size_t i(0); i < m; ++i) {
+		for (size_t j(0); j < n; ++j) {
 			// O(n^1)
-			for (size_t j(0); j < n; ++j) {
-				if (data[i] < data[j]) ++x;
-			}
+			if (data[i] == data[j]) ++x;
 		}
 	}
 	std::printf("[%ld]\n", x);
