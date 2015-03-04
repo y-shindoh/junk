8dde015ebcbfd41bc8dd337aa5465a6a58572723
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-03-04 16:23:27 +0900

    Add new files '20150304_Google_hiring_test'

---
+++ 20150304_Google_hiring_test/FIFO_using_stack.rb
@@ -0,0 +1,59 @@
+#! /usr/bin/ruby -Ku
+# -*- coding: utf-8; tab-width: 4 -*-
+# Author: Yasutaka SHINDOH
+
+class MyDeque
+    def initialize
+        @stack = Array::new
+    end
+
+    def empty?
+        return @stack.empty?
+    end
+
+    def enqueue(value)
+        @stack << value
+    end
+
+    def dequeue
+        if empty? then
+            return nil
+        else
+            return dequeue_subfunction
+        end
+    end
+
+    def dequeue_subfunction
+        last = @stack.pop
+        if empty? then
+            return last
+        else
+            first = dequeue_subfunction
+            @stack << last
+            return first
+        end
+    end
+
+    def print(io=$stdout)
+        io.puts(@stack.to_s)
+    end
+end
+
+deque = MyDeque::new
+
+STDIN.each { |value|
+    value.strip!
+    break if value.empty?
+
+    case value
+    when 'dequeue' then
+        value = deque.dequeue
+        printf("dequeue:\t%s\n", value)
+    else
+        printf("enqueue:\t%s\n", value)
+        deque.enqueue(value)
+    end
+
+    print("print:\t")
+    deque.print
+}
